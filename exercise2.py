73a67051459aa2ea52a7f25e3dc904dbbab01d05
jacobo <jacobogrh@gmail.com>
2018-09-19 13:54:29 +0200

    Hw

---
+++ exercise2.py
@@ -0,0 +1,7 @@
+import math
+num1 = float (input ('Enter the first number: '))
+num2 = float (input ('Enter the second number: '))
+
+div = float (num1 / num2)
+
+print('the rounded up division result is', math.ceil(div))
---
+++ exercise4.py
@@ -0,0 +1,15 @@
+number1 = int (input (" add the first number "))
+number2 = int (input (" add the second number "))
+operator = input (" enter the operator ")
+
+if operator == "+":
+    print(number1,'+',number2,'=', number1 + number2)
+if operator == "-":
+    print(number1, '-', number2, '=', number1, number2)
+
+if operator == "*":
+    print(number1, '*', number2, '=', number1, number2)
+
+if operator == "/":
+    print(number1, '/', number2, '=', number1, number2)
+
